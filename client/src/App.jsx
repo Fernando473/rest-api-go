6d2ebfd110335a8fd57b976dd61e4631cd584cc1
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2023-04-02 14:32:27 -0500

    Upload project with go and ReactJS

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <button onClick={async () => {
+        const response  = await fetch(' http://localhost:3000/users')
+        const data = await response.json()
+        console.log(data)
+      }}> Obtener datos</button>
+    </div>
+  )
+}
+
+export default App
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+	app.Static("/", "./client/dist")
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "usuarios desde el backend",
+		})
+	})
+
+	// Server is working on http://localhost:3000
+	app.Listen(":3000")
+}
