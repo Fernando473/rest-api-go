39faf10fbe7b4632da680b5a57ed457171ef4575
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2023-04-02 14:56:58 -0500

    Proxy fixed to users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={async () => {
-        const response  = await fetch(' http://localhost:3000/users')
+        const response  = await fetch('/users')
         const data = await response.json()
         console.log(data)
       }}> Obtener datos</button>
