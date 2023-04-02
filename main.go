408ff49762c26922368f651bc64214c5cb2d6676
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2023-04-02 14:48:46 -0500

    Port fixed to production

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
 
 	app := fiber.New()
 
@@ -19,5 +26,5 @@ func main() {
 	})
 
 	// Server is working on http://localhost:3000
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
